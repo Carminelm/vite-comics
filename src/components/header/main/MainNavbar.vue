f9bd778e83e22e1b3ca6e18af497437e79e666bc
Carminelm <carminelm54@gmail.com>
2024-07-06 15:45:38 +0200

    add main

---
+++ src/components/header/main/MainNavbar.vue
@@ -0,0 +1,89 @@
+<script>
+export default {
+    name: 'MainNavbar',
+
+    data() {
+        return {
+            navItems: [
+                {
+                    text: 'Digital Comics',
+                    src: `comics-digital-comics.png`
+                },
+
+
+                {
+                    text: 'DC Merchandise',
+                    src: `comics-merchandise.png`
+                },
+
+
+                {
+                    text: 'Subscriptions',
+                    src: `comics-subscriptions.png`
+                },
+
+
+                {
+                    text: 'Comic Shop Locator',
+                    src: `comics-shop-locator.png`
+                },
+
+
+                {
+                    text: 'DC Power Visa',
+                    src: `dc-power-visa.svg`
+                },
+            ]
+        }
+    },
+
+    methods: {
+        getImgPath(imgPath) {
+            return new URL('../../assets/img/buy-' + imgPath, import.meta.url).href
+        }
+    }
+}
+</script>
+
+<template>
+    <nav class="container">
+        <ul class="d-flex justify-around align-center">
+            <li v-for="navItem in navItems">
+                <a href="#" class="d-flex align-center text-uppercase">
+                    <img :src="getImgPath(navItem.src)" :alt="navItem.text">
+                    <span>{{ navItem.text }}</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+@use '../../styles/partials/variables' as *;
+
+
+
+nav {
+    background-color: $complementary--color;
+
+    ul {
+        padding: 1.2rem 0;
+
+        img {
+            width: 37px;
+            margin-right: 1rem;
+        }
+
+        li {
+            &:hover img {
+                filter: brightness(2);
+            }
+
+            a {
+                font-size: 0.7rem;
+                color: $primary--color;
+            }
+        }
+    }
+}
+</style>
