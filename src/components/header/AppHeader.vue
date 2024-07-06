5f66e6e0ce700f8e1d07ee41e6f0132bcb8383a2
Carminelm <carminelm54@gmail.com>
2024-07-06 15:23:20 +0200

    add variables and general

---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,140 @@
+<script>
+export default {
+    name: 'AppHeader',
+
+    data() {
+        return {
+            navItems: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    active: false,
+                },
+
+                {
+                    text: 'Comics',
+                    url: '#',
+                    active: true,
+                },
+
+                {
+                    text: 'Movies',
+                    url: '#',
+                    active: false,
+                },
+
+                {
+                    text: 'TV',
+                    url: '#',
+                    active: false,
+                },
+
+                {
+                    text: 'Games',
+                    url: '#',
+                    active: false,
+                },
+
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    active: false,
+                },
+
+                {
+                    text: 'Videos',
+                    url: '#',
+                    active: false,
+                },
+
+                {
+                    text: 'Fans',
+                    url: '#',
+                    active: false,
+                },
+
+                {
+                    text: 'News',
+                    url: '#',
+                    active: false,
+                },
+
+                {
+                    text: 'Shop',
+                    url: '#',
+                    active: false,
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <nav class="d-flex justify-between align-center">
+                <a class="logo-container" href="#">
+                    <img src="../../assets/img/dc-logo.png" alt="logo" title="logo">
+                </a>
+
+                <ul class="d-flex">
+                    <li class="text-uppercase d-flex" :class="(navItem.active) ? 'active' : ''"
+                        v-for="navItem in navItems">
+                        <a :href="(navItem.url)" :alt="navItem.text">{{ navItem.text }}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style scoped lang="scss">
+@use '../../styles/partials/variables' as *;
+
+
+
+header {
+    background-color: $primary--color;
+
+    .container {
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+
+    nav {
+        height: 100px;
+
+        .logo-container {
+            width: 60px;
+        }
+
+        ul {
+            height: 100%;
+
+            li {
+                margin-right: 2.3rem;
+                line-height: 100px;
+
+                &.active {
+                    border-bottom: 3px solid $complementary--color;
+
+                    a {
+                        color: $complementary--color;
+                    }
+                }
+
+                a {
+                    font-size: 0.7rem;
+                    font-weight: bold;
+                    color: $darker--icon--bg-color;
+
+                    &:hover {
+                        color: $secondary--color;
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
